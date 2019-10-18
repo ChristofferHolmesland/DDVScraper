497e990c9201ac102a71d4ae8951e162088f837e
Christoffer Holmesland <ChristofferHolmesland@users.noreply.github.com>
2019-10-19 01:41:52 +0200

    search multiple muni in a row

--- postmanager.py
+++ postmanager.py
@@ -13,51 +13,56 @@ parser.add_argument("--max_id", type=int)
 parser.add_argument("--n", type=int)
 parser.add_argument("--print", default="all", type=str, choices=["all", "valid", "none"])
 
-args = parser.parse_args()
-municipality = args.municipality
-min_id = args.min_id or -1
-max_id = args.max_id
-n_search = args.n
-print_option = args.print
+def search(municipality, min_id, max_id, n_search, print_option):
+    conn = sqlite3.connect("{}.db".format(municipality))
 
-conn = sqlite3.connect("{}.db".format(municipality))
-
-conn.execute("CREATE TABLE IF NOT EXISTS PostCase(id INTEGER PRIMARY KEY, year INTEGER, number INTEGER, title TEXT, responsible TEXT, status TEXT, htmldocument TEXT)")
-conn.commit()
-
-if min_id == -1:
-    row = conn.execute("SELECT * FROM PostCase ORDER BY id DESC LIMIT 1").fetchone()
-    min_id = 0 if row is None else row[0] + 1
-
-max_range = 0
-if max_id:
-    max_range = max_id + 1
-elif n_search:
-    max_range = min_id + n_search
-
-for i in range(min_id, max_range):
-    data = r.get(URL.format(municipality, i))
-    valid = data.status_code == 200
-
-    if print_option == "all" or (print_option == "valid" and valid):
-        print("Case {0}, status code: {1} {2}".format(i, data.status_code, "----------" if valid else ""))
-    if not valid: continue
-
-    soup = BeautifulSoup(data.text, "html.parser")
-    data_fields = soup.find("div", class_="detailsmetadatablock").find_all("div", class_="display-field")
-    clean_data_fields = [x.text.strip() for x in data_fields]
-
-    year, number = clean_data_fields[0].split("/")
-
-    db_data = (
-        i,
-        int(year),
-        int(number),
-        clean_data_fields[1],
-        clean_data_fields[2],
-        clean_data_fields[3],
-        data.text
-    )
-
-    conn.execute(INSERT_STATEMENT, db_data)
+    conn.execute("CREATE TABLE IF NOT EXISTS PostCase(id INTEGER PRIMARY KEY, year INTEGER, number INTEGER, title TEXT, responsible TEXT, status TEXT, htmldocument TEXT)")
     conn.commit()
+
+    if min_id == -1:
+        row = conn.execute("SELECT * FROM PostCase ORDER BY id DESC LIMIT 1").fetchone()
+        min_id = 0 if row is None else row[0] + 1
+
+    max_range = 0
+    if max_id:
+        max_range = max_id + 1
+    elif n_search:
+        max_range = min_id + n_search
+
+    for i in range(min_id, max_range):
+        data = r.get(URL.format(municipality, i))
+        valid = data.status_code == 200
+
+        if print_option == "all" or (print_option == "valid" and valid):
+            print("{0}: Case {1}, status code: {2} {3}".format(municipality, i, data.status_code, "----------" if valid else ""))
+        if not valid: continue
+
+        soup = BeautifulSoup(data.text, "html.parser")
+        data_fields = soup.find("div", class_="detailsmetadatablock").find_all("div", class_="display-field")
+        clean_data_fields = [x.text.strip() for x in data_fields]
+
+        year, number = clean_data_fields[0].split("/")
+
+        db_data = (
+            i,
+            int(year),
+            int(number),
+            clean_data_fields[1],
+            clean_data_fields[2],
+            clean_data_fields[3],
+            data.text
+        )
+
+        conn.execute(INSERT_STATEMENT, db_data)
+        conn.commit()
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    municipality = args.municipality.split(",")
+    min_id = args.min_id or -1
+    max_id = args.max_id
+    n_search = args.n
+    print_option = args.print
+
+    for m in municipality:
+        search(m, min_id, max_id, n_search, print_option)
